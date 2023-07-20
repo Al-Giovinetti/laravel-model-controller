fed16367e0f138579fc0dd0de199b3af1528e857
Al-Giovinetti <giovinettialessio@gmail.com>
2023-07-20 16:59:40 +0200

    Sistemato scaffolding con guest

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,6 @@
 <?php
 
-use App\Http\Controllers\PageController;
+use App\Http\Controllers\Guest\PageController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,4 +14,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', [PageController::class, "home"])->name("home");
+Route::get('/', )->name("home");
